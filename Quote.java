7346c7f7d2b5104cd252d093c06d914641959fbd
Brian Pomerantz <bbp22@cornell.edu>
2017-04-26 13:19:01 -0400

    added functionality for commodities such as Ag, Au, and Pt

--- Quote.java
+++ Quote.java
@@ -7,6 +7,7 @@ public class Quote {
     private String symbol;
     private float price;
     private float change;
+    private boolean commodity;
 
     private final String ANSI_RESET = "\u001B[0m";
     private final String ANSI_GREEN = "\u001B[32m";
@@ -16,6 +17,13 @@ public class Quote {
         symbol = s;
         price = 0;
         change = 0;
+
+        if (s.equalsIgnoreCase("XAG") || s.equalsIgnoreCase("XAU") || s.equalsIgnoreCase("XPT")) {
+            commodity = true;
+        }
+        else {
+            commodity = false;
+        }
     }
 
     public String getSymbol() {
@@ -34,9 +42,15 @@ public class Quote {
         URL url;
         InputStream is = null;
         BufferedReader br;
-        String line;
-
-        String address = "http://finance.google.com/finance/info?client=ig&q=" + symbol;
+        String line, address;
+        
+        if (commodity) {
+            
+            address = "http://download.finance.yahoo.com/d/quotes.csv?s=" + symbol + "USD=X&f=l1c1d1t1";
+        }
+        else {
+            address = "http://finance.google.com/finance/info?client=ig&q=" + symbol;
+        }
 
         try {
             url = new URL(address);
@@ -47,27 +61,44 @@ public class Quote {
             //    System.out.println(line);
             //}
 
-            for (int i = 0; i < 7; i++) {
+            int lstart, lend, cstart, cend;
+
+            if (commodity) {
                 line = br.readLine();
+
+                lstart = 0;
+                lend = line.substring(lstart).indexOf(',') + lstart;
+
+                cstart = lend + 1;
+                cend = line.substring(cstart).indexOf(',') + cstart;
+
+                price = Float.parseFloat(line.substring(lstart,lend));
+                change = Float.parseFloat(line.substring(cstart,cend));
+            }
+            else {
+                for (int i = 0; i < 7; i++) {
+                    line = br.readLine();
+                }
+
+                line = br.readLine();
+
+                lstart = 12;
+                lend = line.substring(lstart).indexOf('\"') + lstart;
+                
+                price = Float.parseFloat(line.substring(lstart,lend));
+                
+                for (int i = 0; i < 5; i++) {
+                    line = br.readLine();
+                }
+
+                line = br.readLine();
+            
+                cstart = 8;
+                cend = line.substring(cstart).indexOf('\"') + cstart;    
+                
+                change = Float.parseFloat(line.substring(cstart,cend));
             }
 
-            line = br.readLine();
-
-            int lstart = 12;
-            int lend = line.substring(lstart).indexOf('\"') + lstart;
-
-            price = Float.parseFloat(line.substring(lstart,lend));
-            
-            for (int i = 0; i < 5; i++) {
-                line = br.readLine();
-            }
-
-            line = br.readLine();
-            
-            int cstart = 8;
-            int cend = line.substring(cstart).indexOf('\"') + cstart;
-            
-            change = Float.parseFloat(line.substring(cstart,cend));
 
             return true;
         }
